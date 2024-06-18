f1c7fa246c91667254624e4792a68f16ee3c609b
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-18 15:25:38 -0500

    format date.

--- src/NewsCard/NewsCard.js
+++ src/NewsCard/NewsCard.js
@@ -1,9 +1,14 @@
 import './NewsCard.css'
 function NewsCard({ data }) {
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
+  };
     return (
       <div className="news-card">
         <div className='article'>
         <h4>{data.title}</h4>
+        <p>{formatDate(data.publishedAt)}</p>
         <p>By {data.author}</p>
         <p>Source: {data.source.name}</p>
         <p>{data.description}</p>
