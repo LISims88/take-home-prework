252ca0a8b42851640b3bc25c217506d44fbda388
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-18 12:45:05 -0500

    set up the Directory and basic App Views.

---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+function Header(){
+    return(
+        <>
+        <h1>Top News Headlines</h1>
+        </>
+    )
+}
+export default Header
---
+++ src/NewsContainer/NewsContainer.js
@@ -0,0 +1,9 @@
+import NewsCard from '../NewsCard/NewsCard';
+import './NewsContainer.css'
+
+function NewsContainer(){
+    return(
+        <NewsCard/>
+    )
+}
+export default NewsContainer
