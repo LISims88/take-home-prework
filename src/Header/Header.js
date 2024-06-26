c5377fd5fb6b8604f1f7d21d61707a05f6a39961
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-26 16:02:36 -0500

    fixed css styling.

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,14 +2,14 @@ import './Header.css'
 import Select from '../Select/Select'
 function Header({onOptionChange}){
     return(
-        <>
-        <div>
-        <h1>Top News Headlines</h1>
+        <div className='header'>
+            <div>
+            <h1>Top News Headlines</h1>
+            </div>
+            <div>
+            <Select onOptionChange={onOptionChange}/>
+            </div>
         </div>
-        <div>
-           <Select onOptionChange={onOptionChange}/>
-        </div>
-        </>
     )
 }
 export default Header
