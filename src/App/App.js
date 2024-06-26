d39515c1ead808cd2d88cb5b30cdc13eebd06582
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-26 13:27:16 -0500

    Setup App for api usage.

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,31 @@
+import DetailedArticle from '../DetailedArticle/DetailedArticle.js';
 import Header from '../Header/Header';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import getArticles from '../apiCalls.js';
 import mockData from '../mockData.js';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const fetchedArticles = await getArticles();
+        setArticles(fetchedArticles);
+      } catch (error) {
+        console.error('Error fetching Articles:', error);
+      }
+    };
+
+    fetchArticles();
+  }, []);  
   return(
     <>
     <Header data={mockData}/>
-    <NewsContainer data={mockData}/>
+    {/* <NewsContainer data={mockData}/> */}
+    <DetailedArticle data={mockData}/>
     </>
   )
 }
